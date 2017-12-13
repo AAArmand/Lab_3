2456fbb04b5b99418526ae625d4b78491f8a38fc
AAArmand <armand.airapetyan@yandex.ru>
2017-12-13 20:05:32 +0500

    first commit

---
+++ GraphicsEditor/Transformation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsEditor {
+    class Transformation {
+        private Matrix TransformMatrix;
+        private static Transformation transformation = new Transformation();
+
+        /// Возвращает преобразование поворота на угол angle вокруг точки (0,0)
+        public static Transformation Rotate(float angle) {
+            transformation.TransformMatrix.Rotate(angle);
+            return transformation;
+        }
+
+        /// Возвращает преобразование параллельного переноса на вектор ((0,0), point)
+        public static Transformation Translate(PointF point) {
+            transformation.TransformMatrix.Translate(point.X, point.Y);
+            return transformation;
+        }
+
+        /// Возвращает преобразование масштабирования с коэффициентами scaleX и scaleY
+        /// относительно точки (0, 0).
+        /// отрицательные значения параметров соответствуют инверсии
+        public static Transformation Scale(float scaleX, float scaleY) {
+            transformation.TransformMatrix.Scale(scaleX, scaleY);
+            return transformation;
+        }
+
+        /// Возвращает центральное аффинное преобразование, заданное матрицей 2x2
+        public static Transformation FromMatrix(float[] matrix) {
+            transformation.TransformMatrix = new Matrix(matrix[0], matrix[1], matrix[2], matrix[3], 0, 0);
+            return transformation;
+        }
+
+        /// Возвращает преобразование, получающееся последовательным применением
+        /// преобразований a и b
+        public static Transformation operator *(Transformation a, Transformation b) {
+            transformation.TransformMatrix.Multiply(a.TransformMatrix);
+            transformation.TransformMatrix.Multiply(b.TransformMatrix);
+            return transformation;
+        }
+        
+        /// Для любой точки плоскости возвращает её образ
+        public PointF this[PointF point] { get;}
+
+        private Transformation() {
+            TransformMatrix = new Matrix();
+        }
+    }
+}
