b6a55745d11baf461f37686b595b68b420b06dc5
AAArmand <armand.airapetyan@yandex.ru>
2017-12-13 20:17:42 +0500

    change Transformation

--- GraphicsEditor/Transformation.cs
+++ GraphicsEditor/Transformation.cs
@@ -9,16 +9,17 @@ using System.Threading.Tasks;
 namespace GraphicsEditor {
     class Transformation {
         private Matrix TransformMatrix;
-        private static Transformation transformation = new Transformation();
 
         /// Возвращает преобразование поворота на угол angle вокруг точки (0,0)
         public static Transformation Rotate(float angle) {
+            Transformation transformation = new Transformation();
             transformation.TransformMatrix.Rotate(angle);
             return transformation;
         }
 
         /// Возвращает преобразование параллельного переноса на вектор ((0,0), point)
         public static Transformation Translate(PointF point) {
+            Transformation transformation = new Transformation();
             transformation.TransformMatrix.Translate(point.X, point.Y);
             return transformation;
         }
@@ -27,12 +28,14 @@ namespace GraphicsEditor {
         /// относительно точки (0, 0).
         /// отрицательные значения параметров соответствуют инверсии
         public static Transformation Scale(float scaleX, float scaleY) {
+            Transformation transformation = new Transformation();
             transformation.TransformMatrix.Scale(scaleX, scaleY);
             return transformation;
         }
 
         /// Возвращает центральное аффинное преобразование, заданное матрицей 2x2
         public static Transformation FromMatrix(float[] matrix) {
+            Transformation transformation = new Transformation();
             transformation.TransformMatrix = new Matrix(matrix[0], matrix[1], matrix[2], matrix[3], 0, 0);
             return transformation;
         }
@@ -40,13 +43,14 @@ namespace GraphicsEditor {
         /// Возвращает преобразование, получающееся последовательным применением
         /// преобразований a и b
         public static Transformation operator *(Transformation a, Transformation b) {
+            Transformation transformation = new Transformation();
             transformation.TransformMatrix.Multiply(a.TransformMatrix);
             transformation.TransformMatrix.Multiply(b.TransformMatrix);
             return transformation;
         }
-        
+
         /// Для любой точки плоскости возвращает её образ
-        public PointF this[PointF point] { get;}
+        //public PointF this[PointF point] { get; }
 
         private Transformation() {
             TransformMatrix = new Matrix();
