5e58c583a484d493cf1f66f5b87107aff6bfe109
Aarabika <hjvek1@gmail.com>
2017-12-16 16:03:00 +0500

    rewrites

--- GraphicsEditor/ColorCommand.cs
+++ GraphicsEditor/ColorCommand.cs
@@ -8,50 +8,17 @@ using System.Threading.Tasks;
 using DrawablesUI;
 
 namespace GraphicsEditor {
-    class ColorCommand : ICommand {
-        private Picture picture;
+    class ColorCommand :CommandIndex, ICommand {
         public string Name { get { return "color"; } }
 
         public string Help { get { return "Изменяет цвет линий фигуры на заданный"; } }
         public string Description { get { return "Изменяет цвет линий фигуры. Первый параметр - название цвета, последующие - индексы фигур"; } }
         public string[] Synonyms { get { return new string[] { "coloring", "colouring" }; } }
 
-        public ColorCommand(Picture picture) {
-            this.picture = picture;
+        public ColorCommand(Picture picture) :base(picture) {        
         }
 
-        private int[] ValidateIndexes(string[] parameters) {
-            try {
-                List<int> indexes = new List<int>();
-                int index;
-                foreach (string parametr in parameters.Skip(1)) {
-                    index = int.Parse(parametr);
-                    if (index < 0) {
-                        throw new ArgumentException("Индекс " + index + " не может быть отрицательным");
-                    }
-
-                    if (index >= picture.Shapes.Count()) {
-                        throw new ArgumentException("Не существует фигуры с индексом " + index);
-                    }
-
-                    if (indexes.Contains(index)) {
-                        throw new ArgumentException("Индекс " + index + " повторяется");
-                    } else {
-                        indexes.Add(index);
-                    }
-                }
-
-                return indexes.ToArray();
-
-            } catch (FormatException) {
-                Console.WriteLine("Вы ввели индексы в неверном формате");
-            } catch (OverflowException) {
-                Console.WriteLine("Вы ввели слишком большое число в качестве индекса");
-            } catch (ArgumentException error) {
-                Console.WriteLine(error.Message);      
-            }
-             return null;
-        }
+        
 
         public void Execute(params string[] parameters) {
             try {
